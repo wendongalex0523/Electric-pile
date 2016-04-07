81f33bbbe23560185c7d95227e8648530659a83b
chenhang <371726787@qq.com>
2016-04-07 15:45:56 +0800

    2
    2

---
+++ Electric pile/Electric pile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Electric pile
+//
+//  Created by 温冬 on 16/4/7.
+//  Copyright © 2016年 PowerLife Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Electric pile/Electric pile/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Electric pile
+//
+//  Created by 温冬 on 16/4/7.
+//  Copyright © 2016年 PowerLife Corporation. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
